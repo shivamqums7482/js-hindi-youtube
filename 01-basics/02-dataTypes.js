681ef9535ce0590c1b6d6b1b6282fbbfa6ebafe4
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-24 07:15:28 +0000

    dataypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Hitesh")
+
+
+    let name = "hitesh"
+    let age = 18
+    let isLoggedIn = false
+
+
+    // number = 2 to power 53 or 52  is the range
+    // bigint
+    // string = ""
+    // boolean = true/false
+    // null = standalone value for ex let state = null
+    // undefined = //for ex let state;
+    // symbol = unique
+
+
+    // object
+
+    // console.log(typeof "hitesh");
+    // console.log(typeof age);
+    // console.log(typeof null); output = object
+    console.log(typeof undefined); // output=undefined
+    
