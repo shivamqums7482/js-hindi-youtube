8b38f27fea9893221af02f902d96b193d6b75334
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-25 14:34:41 +0000

    comparison

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1); output will be true or false
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//output = true,true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+//output = false,false,true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//output = false,false,true
+
+// strict check means ===
+
+// console.log("2" == 2);
+//output = true
+
+// console.log("2" === 2);
+//output = false
