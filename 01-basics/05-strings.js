18be4d5d51a10d8945b04ed29fcac7bad1c519d5
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-26 10:45:39 +0000

    strings

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,48 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");  //output = hitesh50 Value
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+//output = Hello my name is hitesh and my repo count isn 50
+
+// const gameName = new String('hiteshhc')
+// console.log(gameName[0]);  //output = h
+// console.log(gameName.__proto__);  //output = {}
+
+
+// console.log(gameName.length);  //output = 8
+// console.log(gameName.toUpperCase());  //output = HITESHHC
+// console.log(gameName.charAt(2));  //output = t
+// console.log(gameName.indexOf('t'));  //output = 2
+
+
+// const gameName = new String('hitesh-hc')
+// const newString = gameName.substring(0, 4)
+// console.log(newString);  //output = hite
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);  //output = ite
+
+// const newStringOne = "   hitesh   "
+// console.log(newStringOne);  //output =    hitesh   
+// console.log(newStringOne.trim());  // output = hitesh
+
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20', '-'))  //output = https://hiteshh.com/hitesh-choudhary
+
+// console.log(url.includes('hitesh'))  //output = true
+
+// console.log(url.includes('sundar'))  //output = false
+
+const gameName = new String('hitesh-hc-com')
+console.log(gameName.split('-'));  //output = [  'hitesh',  'hc',  'com'  ]
+
+
+
+
+
+
+
+
