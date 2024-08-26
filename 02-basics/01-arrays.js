4c042809de5102614d309051b2d9d7d00203a064
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-26 18:49:53 +0000

    arrays 1

---
+++ 02-basics/01-arrays.js
@@ -0,0 +1,75 @@
+//+++++++++Array+++++++++++
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeroes = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);  //output = 1
+
+//Array Methods
+
+// myArr.push(6)
+// console.log(myArr);  //output = [
+   // 0, 1, 2, 3,
+   // 4, 5, 6
+  // ]
+
+// myArr.push(6)
+// myArr.pop()
+// console.log(myArr);  //output = [ 0, 1, 2, 3, 4, 5 ]
+
+// myArr.unshift(9)
+// console.log(myArr);  //output = [
+   // 9, 0, 1, 2,
+   // 3, 4, 5
+ // ]
+
+//  myArr.shift()
+//  console.log(myArr);  //output = [ 1, 2, 3, 4, 5 ]
+
+
+// console.log(myArr.includes(9));  //output = false
+// console.log(myArr.indexOf(9));  //output = -1
+// console.log(myArr.indexOf(3));  //output = 3
+// console.log(myArr);  //output = [ 0, 1, 2, 3, 4, 5 ]
+
+
+// const newArr = myArr.join()
+// console.log(myArr);  //output = [ 0, 1, 2, 3, 4, 5 ]
+// console.log(newArr);  //output = 0,1,2,3,4,5
+// console.log(typeof newArr);  //output = string
+
+
+//slice, splice
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+
+// console.log("A", myArr); //output = A [ 0, 1, 2, 3, 4, 5 ]
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);  //output = [ 1, 2 ]
+// console.log("B ",myArr);  //output = B  [ 0, 1, 2, 3, 4, 5 ]
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);  //output = C  [ 0, 4, 5 ]
+// console.log(myn2);  //output = [ 1, 2, 3 ]
+
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+
+console.log(marvel_heros);
+
+
+
+
+
+
+
+
+
+
+  
+
