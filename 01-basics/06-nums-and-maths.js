e534132f0a3e99b67c33e1accf8a54398a6ac383
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-26 11:30:26 +0000

    math and num

---
+++ 01-basics/06-nums-and-maths.js
@@ -0,0 +1,69 @@
+const score = 400
+// console.log(score);  //output = 400
+
+
+// const balance = new Number(100)
+// console.log(balance);  //ouput = [Number: 100]
+
+// console.log(balance.toString())  //output = 100
+// console.log(balance.toString().length);  //output = 3
+// console.log(balance.toFixed(2));  //output = 100.00
+// console.log(balance.toFixed(1));  //output = 100.0
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));  //ouput = 23.9
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));  //output = 124
+
+// const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(3));  //ouput = 1.12e+3
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));  //ouput = 123.9
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());  //output = 1,000,000
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  //output = 10,00,000
+
+
+
+//+++++++++++++++++MATHS++++++++++++++++++
+
+// console.log(Math);  //output = Object [Math] {}
+
+// console.log(Math.abs(-4));  //ouput = 4
+// console.log(Math.abs(4));  //ouput = 4
+// console.log(Math.round(4.3));  //output = 4
+// console.log(Math.round(4.6));  //ouput = 5
+// console.log(Math.ceil(4.2));  //ouput = 5
+// console.log(Math.floor(4.9));  //output = 4
+// console.log(Math.min(4,3,6,8,9));  //output = 3
+// console.log(Math.max(4,3,6,8,9));  //ouput = 9
+
+// console.log(Math.random());  //ouput = after runnning again and again the value is between 0 and 1 that is 0.8753566535 and 0.6554355321 and 0.8365533245
+console.log(Math.random()*10);  //oupput = after running again and again = 0.10040199563167618 & 1.319044792075299 and 0.8313984910804884 & 6.4422197233212675 and 0.41187967655393165 & 4.844377601790059
+
+console.log(Math.random()*10 + 1); //output = 9.2856464565
+console.log(Math.floor(Math.random()*10) + 1);  //output = 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  //ouput = 17 and 15 and 19 = after running again and again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
