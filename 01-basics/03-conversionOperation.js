b2fd8dc5d8099a06e5e2c398827dca5aafa9d0d8
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2024-08-26 09:44:53 +0000

    datatypes summary

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "hitesh"
+// let score = "Hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" = 33
 //"33abc" = NaN = not a number
@@ -15,10 +15,10 @@ let valueInNumber = Number(score)
 
 
 
-let isLoggedIn = "hitesh"
+// let isLoggedIn = "hitesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //0 = false
 //1 = true
@@ -26,9 +26,9 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //"hitesh" = true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
@@ -37,16 +37,23 @@ let stringNumber = String(someNumber)
 
 // ***********operations***************
 
-let value = 3
-let negValue = -value
-// console.log(negValue);  //output = -3
+// let value = 3
+// let negValue = -value
+// console.log(negValue);  
+//output = -3
 
-// console.log(2+2);  output = 4
-// console.log(2-2);  output = 0
-// console.log(2*2);  output = 4
-// console.log(2**3);  output = 8
-// console.log(2/3);  output = 0.666666666666
-// console.log(2%3);  output = 2
+// console.log(2+2);  
+// output = 4
+// console.log(2-2);  
+//output = 0
+// console.log(2*2);  
+//output = 4
+// console.log(2**3);  
+//output = 8
+// console.log(2/3); 
+// output = 0.666666666666
+// console.log(2%3);  
+//output = 2
 
 // let str1 = "hello"
 // let str2 = " hitesh"
@@ -57,25 +64,31 @@ let negValue = -value
 //output = hello hitesh
 
 // console.log("1" + 2);
+//output = 12
 // console.log(1 + "2");
+//output = 12
 // console.log("1" + 2 + 2);
+//output = 122
 // console.log(1 + 2 + "2");
+//output = 32
 
 // console.log((3 + 4) * 5 % 3);
 
 // console.log(+true); 
-// console.log(+"");
+//output = 1
+// console.log(+"")
+//output = 0
 
 
 //output = 12 and 12 and 122 and 32 and 5 and true = true and +true = 1
 //output = (+"") = 1 and 0
 
-let num1, num2, num3
-num1 = num2 = num3 = 2+2
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);   
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);   
 
 //gameCounter++ = output = 101 and ++gameCounter = 101
 
--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -3,34 +3,34 @@
 //7 types = 1.String 2.Number 3.Union 4.Null 5.Boolean 6.Undefined 7.Symbol
 //8.BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail; //output = undefined value
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail; //output = undefined value
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);  //output = false
+// console.log(id === anotherId);  //output = false
 
-const bigNumber = 4357233752526526162n
+// const bigNumber = 4357233752526526162n
 
 
 //Reference type (Non-primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
 // console.log(typeof bigNumber);  //output = undefined
 
 // console.log(typeof outsideTemp);  //output = object
@@ -40,6 +40,45 @@ const myFunction = function(){
 // console.log(typeof myFunction);  //output = function
 
 // console.log(typeof anotherId);  //output = symbol
+
+
+
+//+++++++++++MEMORY+++++++++++++
+
+//2 type of memory = stack memory and heap memory
+
+//stack memory is used in primitve type and heap is used in non-primitive type
+
+// let myYoutubename = "hiteshchoudharydotcom"
+// let anothername = myYoutubename
+
+// console.log(anothername);  //output = hiteshchoudharydotcom
+
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);  //output = hiteshchoudharydorcom
+console.log(anothername);  //output = chaiaurcode
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
 
 
 
